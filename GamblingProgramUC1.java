089e247d39f7d17932081315d71e59f74d7ffcbd
SmitaDugur <smitadugur25@gmail.com>
2021-12-15 08:37:18 +0530

    ADDED:Program

---
+++ GamblingProgramUC1.java
@@ -0,0 +1,13 @@
+public  class GamblingProgramUC1 {
+
+  public static void main(String[] args) {
+    {
+       public static int gamblerAmountDay = 100;
+        public static int gamblerHalfAmount = 50;
+        public static int betEachGame = 1;
+        public static int totalDays = 20;
+        
+        {
+                GamblerBet();
+           
+        }
